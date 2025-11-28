bdd10595fdd201eb014397a5a14fe56b241c05cc
Akseli Muhonen <ksenox@ksenoxdeb.v6.elisa-mobile.fi>
2025-11-28 21:00:13 +0200

    Auto-commit 2025-11-28_21:00:01

--- lua/plugins/taskwarrior-nvim.lua
+++ lua/plugins/taskwarrior-nvim.lua
@@ -1,5 +1,10 @@
 return {
-  -- 'ribelo/taskwarrior.nvim',
-  -- dependencies = { 'nvim-lua/plenary.nvim' },
-  -- opts = {},
+  'ribelo/taskwarrior.nvim',
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  -- or
+  config = true,
 }
